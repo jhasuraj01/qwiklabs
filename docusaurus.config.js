7edaca6c5cc0932d9d75225829fd9dc31e00a492
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2021-10-03 15:16:21 +0000

    added seo tags

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/jhasuraj01/qwiklabs/tree/main/',
-          showLastUpdateTime: true,
+          showLastUpdateTime: true
         },
         // blog: {
         //   showReadingTime: true,
