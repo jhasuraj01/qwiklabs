dfd11d14525d61ba154136ca3b416fab240e6c24
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2021-10-03 10:21:23 +0000

    added github action and edit option

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          // editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/jhasuraj01/qwiklabs/tree/main/',
         },
         blog: {
           showReadingTime: true,
